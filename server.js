166c71bff9dd0a52e455f50a9ed8c646f81c3371
Francis Distor <francis.distor@gmail.com>
2019-01-02 19:48:47 -0800

    Fixed port.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const morgan = require('morgan');
 const request = require('request');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, './public')));
